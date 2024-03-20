ae3abb2d880bbb782d029df03d5b5fd524523584
Svitlana Larkina <mrs.liliths@gmail.com>
2024-03-20 18:32:46 +0200

    feat: task 1

--- Homework_03/Homework_03/Program.cs
+++ Homework_03/Homework_03/Program.cs
@@ -4,9 +4,26 @@ namespace Homework_03
 {
     class MainClass
     {
+        public static void task_1()
+        {
+            int[] array = new int[50];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i * 2;
+            }
+
+            // int[] array2 = Enumerable.Range(0, 50).Select(x => x * 2).ToArray();
+
+            foreach (int element in array)
+            {
+                Console.WriteLine($"Element: {element}");
+            }
+        }
         public static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
+            task_1();
         }
     }
 }
