411e3a7b7445276f73072ba1c3e0f9b6b53495fd
Svitlana Larkina <mrs.liliths@gmail.com>
2024-03-20 18:36:41 +0200

    feat: task 3

--- Homework_03/Homework_03/Program.cs
+++ Homework_03/Homework_03/Program.cs
@@ -4,9 +4,41 @@ namespace Homework_03
 {
     class MainClass
     {
+        public static void task_3()
+        {
+            Console.Write("Введіть текст: ");
+            string inputText = Console.ReadLine();
+            int characterCount = inputText.Length;
+            Console.WriteLine($"Кількість символів у введеному рядку: {characterCount}");
+
+
+            Console.Write("Введіть символ для пошуку у рядку: ");
+            char targetChar = Console.ReadLine()[0];
+
+            int countCaseInsensitive = 0;
+            int countCaseSensitive = 0;
+
+            foreach (char c in inputText)
+            {
+                if (char.ToLower(c) == char.ToLower(targetChar))
+                {
+                    countCaseInsensitive++;
+                }
+
+                if (c == targetChar)
+                {
+                    countCaseSensitive++;
+                }
+            }
+
+            Console.WriteLine($"Знайдено {countCaseInsensitive} разів символ '{char.ToLower(targetChar)}' або '{char.ToUpper(targetChar)}'.");
+            Console.WriteLine($"Знайдено {countCaseSensitive} разів символ '{char.ToLower(targetChar)}'.");
+
+        }
         public static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
+            task_3();
         }
     }
 }
